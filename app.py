f16585a056debe184162625964be2f49778d09d8
TandanKumar <tandankumar90@gmail.com>
2023-07-20 20:54:45 +0530

    simple

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from MARKET_BASKET_PROJECT.logger import logging
+import os
 
 app= Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
 def index():
 
+    
     logging.info("Executing the code this is the test for log file")
     return "This is the project about market basket analysis"
 
