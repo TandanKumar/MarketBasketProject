f8133b841fa4499898a44e7434f97ceddcd88d48
TandanKumar <tandankumar90@gmail.com>
2023-07-24 19:59:38 +0530

    updating the entity.config_entity.py

--- MARKET_BASKET_PROJECT/entity/config_entity.py
+++ MARKET_BASKET_PROJECT/entity/config_entity.py
@@ -12,3 +12,5 @@ ModelTrainerConfig = namedtuple("ModelTrainerConfig",
                                  ["trained_model_file_path","base_accuracy" ])
 
 ModelEvaluationConfig =  namedtuple("ModelEvaluationConfig",["model_evaluation_file_path","time_stamp"])
+
+ModelPusherConfig = namedtuple("ModelPusherConfig",["export_dir_path"])
